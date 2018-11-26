c823c11eb65e214dda1ca06d0df0714787fb9b49
David Bernhard <davidbernhard@live.com>
2018-11-26 13:33:51 +0000

    create h2

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ get_header(); ?>
 <div class="wrap">
 	<div class=" homepage-content">
 		<h1><?php esc_html_e( 'THIS IS A BLANK TEMPLATE' ); ?></h1>
+		<h2><?php esc_html_e( 'Dave made an h2' ); ?></h2>
 		<p>
 			<?php
 				esc_html_e("If the background is pink, you're in the right place" );
